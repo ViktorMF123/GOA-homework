39ba9b4999ade3e2ff4431df91100909b605a447
ViktorMF123 <danderspritz978@gmail.com>
2026-02-08 11:45:45 +0400

    second commit

---
+++ homework2.py
@@ -0,0 +1,10 @@
+name = "viktor"
+sname = "maisuradze fier"
+age = 12
+country = "Georgia"
+city = "Tbilisi"
+print("name:" ,name, "/ "
+      "second name:" ,sname, "/ "
+      "age:" ,age, "/ "
+      "country:" ,country, "/ "
+      "city:" ,city)
