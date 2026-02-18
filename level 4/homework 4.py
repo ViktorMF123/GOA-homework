71b37305a768d59f1fa004bcc101f6c0d8db1234
ViktorMF123 <danderspritz978@gmail.com>
2026-02-19 01:48:07 +0400

    commit

---
+++ level 4/homework 4.py
@@ -0,0 +1,54 @@
+#1
+
+# კონკატინაცია არის ტექსტის ერთად ჩვენების ტექნიკა
+
+#2
+
+name = "Viktor"
+sname = "Maisuradze Fier"
+country = "Georgia"
+fav_subject = "Technology"
+
+print("Hello my name is"+" "+name+" "+sname+" "+"I am from"+" "
+      +country+" "+"and my favorite subject is"+" "+fav_subject)
+
+print(" ") # design
+
+#3
+
+Group_94 = "Group 94"
+
+print("I am in"+" "+Group_94)
+
+print(" ") # design
+
+#3
+
+# სამი ნაბიჯი არის კოდის შექმნა, ტესტირება და გამართვა
+
+print(" ") # design
+
+#5
+
+# debugging არის პროცესი სადაც ვასწორებთ არასწორ კოდს და ვასწორებთ მათ
+
+#6
+
+academy1 = 'GOA'
+group = '94'
+print(academy1)
+print(group)
+
+print(" ") # design
+
+#7
+
+a=24
+b=20
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
