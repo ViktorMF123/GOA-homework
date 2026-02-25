e51d23b8fc05e2a6452192dd1f3be64f9676c102
ViktorMF123 <danderspritz978@gmail.com>
2026-02-25 18:36:22 +0400

    commit

---
+++ level 6/homework 6.py
@@ -0,0 +1,23 @@
+# 1 input()-ის ფუნქცია ის არის რომ ის მომხმარებელს საშუალებას აძლევს ტერმინალთან კონტაქტს.
+
+# 2
+a=input("Enter your name: ")
+b=input("Enter your second name: ")
+c=float(input("Enter your age: "))
+print("Hello",a,b,"you are ",c,"years old")
+
+# 3 input-სა და output-ს შორის განსხვავება ისაა რომ მოწყობილობა input-ს აგზავნის ან იღებს,
+#  ხოლო output პირიქითაა ის გვეუბნება.
+
+# 4 123 ფუნქცია გამოიყენება ტექსტის ტიპის დასადგენად, მაგალითად:
+test0 = 200
+test1 = 2.7
+test2 = "34"
+print(type(test0))
+print(type(test1))
+print(type(test2))
+
+# 5
+# ჩვენ ამას იმიტომ ვაკეთებთ რომ მოწყობილობამ ამოიცნოს თუ რა ენაზეა კოდირებული ეს პროგრამა.
+
+#6
