58d686062e4a901f9b8611a44725f2aee21487da
ViktorMF123 <danderspritz978@gmail.com>
2026-03-01 20:17:58 +0400

    commit

---
+++ level 8/classwork 8.py
@@ -0,0 +1,10 @@
+#int() ფუნქცია რიცხვებს სრულ რიცხვებად გარდაქმნის, float რიცხვები კი არასრულ რიცხვებად გარდაქმნის მაგალითად: 20.3, str() ნებისმიერ რამეს სტრიქონის ტიპად გარდაქმნის,
+#ამ ფუნქციების გამოყენება სხვა მიზნებისთვისაც შეგვიძლია.
+clswork=float(input("Enter a number: "))
+smth=float(input("Enter another number: "))
+print(clswork+smth)
+print(clswork-smth)
+print(clswork*smth)
+print(clswork/smth) # float
+print(clswork//smth) # int
+print(clswork**smth)
